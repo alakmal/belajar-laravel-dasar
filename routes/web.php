98b77c05ccde0b260c5d4549383a7c1e5a20d35b
alyouma <alyoumaakmal4@gmail.com>
2025-06-07 11:16:18 +0700

    fix main

--- routes/web.php
+++ routes/web.php
@@ -17,34 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get("/pzn", function () {
-    return "Hello Programmer Zaman Now";
-});
-
-Route::redirect("/youtube", "/pzn");
-
-Route::get("/products/{id}", function ($id) {
-    return "Products : " . $id;
-})->name("products.detail");
-
-Route::get("/products/{product}/items/{item}", function ($product, $item) {
-    return "Products : " . $product . ", Items : " . $item;
-})->name("products.items.detail");
-
-Route::get("/categories/{id}", function (string $categoriId) {
-    return "Categories : " . $categoriId;
-})->where("id", "[0-9]+")
-    ->name("category.detail"); // Only allow numeric IDs for categories
-
-
-Route::get("/produk/{id}", function ($id) {
-    $link = route("products.detail", ["id" => $id]);
-    return "Link : " . $link;
-});
-
-Route::get("/produk-redirect/{id}", function ($id) {
-    return redirect()->route("products.detail", ["id" => $id]);
-})->name("produk.redirect");
-
-Route::get("/controller/hello/{name}", [HelloController::class, "hello"]);
